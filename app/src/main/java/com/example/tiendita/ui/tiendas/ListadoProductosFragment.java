e61bd404bd2940d20fe937a8f85010c8174e9849
JuanButronC <jon4589@hotmail.com>
2021-01-08 11:49:25 -0600

    Cambios IU Corrección
    Se generó un fragment, único para el listado de productos

--- app/src/main/java/com/example/tiendita/ui/tiendas/ListadoProductosFragment.java
+++ app/src/main/java/com/example/tiendita/ui/tiendas/ListadoProductosFragment.java
@@ -25,7 +25,7 @@ public class ListadoProductosFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.listado_productos_fragment, container, false);
+        return inflater.inflate(R.layout.fragment_listado_productos, container, false);
     }
 
     @Override
